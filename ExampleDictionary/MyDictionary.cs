2975410e7805d7fd64a29802cb241c844d438002
Özkan Yılmaz <ozkanyilmaz12@icloud.com>
2021-01-30 18:03:54 +0300

    Add project files.

---
+++ ExampleDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleDictionary
+{
+    class MyDictionary<tK, tV>
+    {
+        tK[] keys; 
+        tV[] values;
+        
+        public MyDictionary()
+        {
+            keys = new tK[0];
+            values = new tV[0];
+
+        }
+        public void Add(tK key, tV value)
+        {
+            tK[] tempKey = keys;
+            tV[] tempValue = values;
+            keys = new tK[keys.Length + 1];
+            values = new tV[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public tK[] Keys
+        {
+            get { return keys; }
+        }
+        public tV[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ ExampleDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExampleDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "The Originals");
+            myDictionary.Add(2, "Sense 8");
+            myDictionary.Add(3, "Supernatural");
+            myDictionary.Add(4, "Teen Wolf");
+            myDictionary.Add(5, "How I Mether Your Mother");
+            myDictionary.Add(6, "Dark");
+
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[0] + " Dizi Adı: " + myDictionary.Values[0]);
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[1] + " Dizi Adı: " + myDictionary.Values[1]);
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[2] + " Dizi Adı: " + myDictionary.Values[2]);
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[3] + " Dizi Adı: " + myDictionary.Values[3]);
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[4] + " Dizi Adı: " + myDictionary.Values[4]);
+            Console.WriteLine("Dizi ID: " + myDictionary.Keys[5] + " Dizi Adı: " + myDictionary.Values[5]);
+        }
+    }
+}
